a2617a029071ad0c3d911fef8671ef6438ec4d9e
Hasan Mostafa <hasanalimostafa1@gmail.com>
2026-02-09 03:35:05 +0300

    Use res.sendFile() method to send files

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 const express = require("express");
-const fs = require("node:fs/promises");
+const path = require("node:path");
 
 const app = express();
 
 app.get("/", async (req, res) => {
-  const body = await generateResponseData("index");
-  res.send(body);
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/about", async (req, res) => {
-  const body = await generateResponseData("about");
-  res.send(body);
+  res.sendFile(path.join(__dirname, "about.html"));
 });
 
 app.get("/contact-me", async (req, res) => {
-  const body = await generateResponseData("contact-me");
-  res.send(body);
+  res.sendFile(path.join(__dirname, "contact-me.html"));
 });
 
 app.use("/", async (req, res) => {
-  const body = await generateResponseData("404");
-  res.status(404).send(body);
+  res.status(404).sendFile(path.join(__dirname, "404.html"));
 });
 
 const PORT = process.env.PORT || 3000;
@@ -30,12 +26,3 @@ app.listen(PORT, (error) => {
   }
   console.log(`My Express app - listening on port ${PORT}!`);
 });
-
-async function generateResponseData(fileName) {
-  try {
-    const data = await fs.readFile(`./${fileName}.html`, { encoding: "utf8" });
-    return data;
-  } catch (err) {
-    console.error(err);
-  }
-}
