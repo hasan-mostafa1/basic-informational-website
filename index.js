fb5500f73880ed0c88b4136d1f6b3591b8236214
Hasan Mostafa <hasanalimostafa1@gmail.com>
2026-02-09 03:10:48 +0300

    Rewrite index.js using Express.js instead of Node

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
-const http = require("node:http");
+const express = require("express");
 const fs = require("node:fs/promises");
 
-const server = http.createServer();
+const app = express();
 
-server.on("request", async (req, res) => {
-  const requestUrl = new URL(`http://localhost:8000${req.url}`);
-  const path = requestUrl.pathname;
-  const body = await generateResponseData(path);
-  const statusCode =
-    path !== "/" && path !== "/about" && path !== "/contact-me" ? 404 : 200;
-  res.writeHead(statusCode, {
-    "Content-Type": "text/html",
-    "Content-length": body.length,
-  });
-  res.end(body);
+app.get("/", async (req, res) => {
+  const body = await generateResponseData("index");
+  res.send(body);
 });
 
-server.listen(8000);
+app.get("/about", async (req, res) => {
+  const body = await generateResponseData("about");
+  res.send(body);
+});
 
-async function generateResponseData(path) {
-  let fileName;
-  if (path == "/") {
-    fileName = "./index.html";
-  } else if (path == "/about" || path == "/contact-me") {
-    fileName = `.${path}.html`;
-  } else {
-    fileName = "./404.html";
+app.get("/contact-me", async (req, res) => {
+  const body = await generateResponseData("contact-me");
+  res.send(body);
+});
+
+app.use("/", async (req, res) => {
+  const body = await generateResponseData("404");
+  res.status(404).send(body);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
   }
+  console.log(`My Express app - listening on port ${PORT}!`);
+});
 
+async function generateResponseData(fileName) {
   try {
-    const data = fs.readFile(fileName, { encoding: "utf8" });
+    const data = await fs.readFile(`./${fileName}.html`, { encoding: "utf8" });
     return data;
   } catch (err) {
     console.error(err);
